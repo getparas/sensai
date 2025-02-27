bee13ec93f3a912785c69e7864b3ecaa70e31c7e
Pritha Karki <prithakarki030@gmail.com>
2025-02-27 13:02:59 +0545

    Add ShadCN components and implement dark mode

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
       <h1>Sensei</h1>
-      <Button>Process</Button>
     </div>
   );
 }
